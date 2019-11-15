ee85b96000bed07b978923e2f4d3ead54dde4237
Suzy Nakayama <suzynakayama@Suzys-MacBook-Pro.local>
2019-11-15 13:21:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/*<Logo />
+      <ImageLinkForm />
+      <FaceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
